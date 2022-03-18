cf859935d095da8164740b38acc4858d20d15f89
dirthoglin <nounpatroon@gmail.com>
2022-03-18 15:03:42 -0500

    Interacting with the user

--- linuxmaud/d_main.c
+++ linuxmaud/d_main.c
@@ -16,22 +16,84 @@ DESCRIPTION:
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
 
 #include <d_main.h>
 #include <l_converter.h>
 
 int maudmain()
 {
-  printf("%i\n", toDeci(2,"1000"));
-  printf("%s\n", fromDeci(2, 8));
+  int frombase = 10, tobase = 2;
+  char* option_selected = malloc(10 * sizeof(char));
 
-  O_Menu(
-    "Main menu",
-    "Exit",
-    "Option 1",
-    "Option 2",
-    NULL
-    );
+  while(1)
+  {
+    O_Menu(
+      "Main menu",
+      "Exit",
+      "Converter",
+      "Calculator",
+      NULL
+      );
+
+    printf("Write an option: "); scanf("%s", option_selected);
+
+    if(!strcmp(option_selected, "Exit"))
+      exit(EXIT_SUCCESS);
+    else if(!strcmp(option_selected, "Converter"))
+    {
+      int T;
+      char* number = malloc(10 * sizeof(char));
+      
+      puts("Note: Write test cases, Write in the firts line the numbers");
+      puts("Note: (i.e. '1 2 4 8 255')");
+
+      printf("From base: %i\nTo base:%i\n", frombase, tobase);
+      printf("Write test cases: "); scanf("%i", &T);
+      while(T--)
+      {
+        scanf("%s", number);
+        strcpy(number, fromDeci(tobase, toDeci(frombase, number)));
+        printf("Result: %s\n", number);
+      }
+
+      free(number);
+    }
+    else if(!strcmp(option_selected, "Calculator"))
+    {
+      int T, number;
+      char* n1 = malloc(10 * sizeof(char));
+      char* n2 = malloc(10 * sizeof(char));
+      char* opr = malloc(10 * sizeof(char));
+      
+      puts("Note: Write test cases, Write in the firts line the firts number, opr and second number");
+      puts("Note: (i.e. '1 + 2', '254 + 1')");
+      puts("Note: More(+) Less(-) Multiplication(*) Division(/)");
+
+      printf("From base: %i\n", frombase);
+      printf("Write test cases: "); scanf("%i", &T);
+      while(T--)
+      {
+        scanf("%s %s %s", n1, opr, n2);
+
+        number = toDeci(frombase, n1);
+        if(!strcmp(opr, "+")) number += toDeci(frombase, n2);
+        else if(!strcmp(opr, "-")) number -= toDeci(frombase, n2);
+        else if(!strcmp(opr, "*")) number *= toDeci(frombase, n2);
+        else if(!strcmp(opr, "/")) number /= toDeci(frombase, n2);
+        else
+          I_Error("Calculator: '%s' That's not an operator", opr);
+
+        printf("Result: %s\n", fromDeci(frombase, number));
+      }
+
+      free(n1); free(n2); free(opr);
+    }
+    else
+      puts("That's not an option");
+
+    puts("\n");
+  }
   
   return 0;
 }
@@ -65,7 +127,7 @@ void I_Error(char *error, ...)
     va_end(argptr);
 
     fflush(stderr);
-    exit(-1);
+    exit(EXIT_FAILURE);
 }
 
 /*=====  End of main body  ======*/
